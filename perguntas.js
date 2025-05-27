018e805631918d09b3656b05cb60f6c7212ad473
LUANAAAAAAAPERUZZZZZZZ <luana.peruzzo@escola.pr.gov.br>
2025-05-27 22:31:28 +0000

    oitavo commit

--- perguntas.js
+++ perguntas.js
@@ -16,3 +16,58 @@ criaCartao(
     'Qual é a principal floresta de araucárias preservada no Paraná?',
     'Floresta com Araucárias, principalmente encontrada na Região Sul do estado.'
 )
+
+criaCartao(
+    'Geografia',
+    ' Qual é o maior rio que forma a fronteira oeste do Paraná?',
+    'Rio Paraná.'
+)
+
+criaCartao(
+    'Energia',
+    'Qual é a famosa usina hidrelétrica localizada na fronteira entre o Paraná e o Paraguai?',
+    'Usina de Itaipu.'
+)
+
+criaCartao(
+    'Cidades',
+    'Qual é a capital do estado do Paraná?',
+    'Curitiba.'
+)
+
+criaCartao(
+    'Geografia',
+    'Em que parque estadual está localizado o Pico Paraná, o ponto mais alto do estado?',
+    ' Parque Estadual Pico Paraná.'
+)
+
+
+criaCartao(
+    'Cidades',
+    'Qual cidade paranaense é conhecida por seu centro histórico preservado e arquitetura colonial?',
+    'Lapa.'
+)
+
+criaCartao(
+    'Cidades',
+    'O que se pode visitar no bairro Santa Felicidade, em Curitiba?',
+    ' Restaurantes típicos italianos, vinícolas, lojas de artesanato e casas históricas.'
+)
+
+criaCartao(
+    'Turismo',
+    'Qual é a importância da Estrada de Ferro Curitiba-Paranaguá para o turismo?',
+    'É uma das viagens de trem mais bonitas do Brasil, passando por paisagens da Serra do Mar com pontes, túneis e matas preservadas.'
+)
+
+criaCartao(
+    'Turismo',
+    'Quais são os principais pontos turísticos de Curitiba?',
+    'ardim Botânico, Ópera de Arame, Museu Oscar Niemeyer, Parque Tanguá, Largo da Ordem e parque barigui.'
+)
+
+criaCartao(
+    'Turismo',
+    'Que atrativo turístico de Curitiba é conhecido por seu teatro de vidro?',
+    'Ópera de Arame.'
+)
