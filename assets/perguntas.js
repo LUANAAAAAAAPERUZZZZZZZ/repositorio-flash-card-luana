fa40c5e99bc6da2dbdde140bc349b246c3eef609
LUANAAAAAAAPERUZZZZZZZ <luana.peruzzo@escola.pr.gov.br>
2025-05-27 21:05:11 +0000

    quinto commit

---
+++ assets/perguntas.js
@@ -0,0 +1,20 @@
+CriaCartao(
+    "História",
+    "Que influência europeia é forte na cultura do Paraná, especialmente no interior?", 
+    "Influência de imigrantes alemães, italianos, ucranianos e poloneses."
+
+
+CriaCartao(
+    "Turismo",
+   "Que importante atração turística natural está localizada em Foz do Iguaçu, no Paraná?", 
+    "Cataratas do Iguaçu."
+
+"
+)
+
+
+CriaCartao(
+    "Geografia",
+    "Qual é a principal floresta de araucárias preservada no Paraná?", 
+   "Floresta com Araucárias, principalmente encontrada na Região Sul do estado."
+)
